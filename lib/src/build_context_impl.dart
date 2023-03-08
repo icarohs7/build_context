9fb8c00eabfd7e7c72adddc160a58db05c3536dd
Icaro Temponi <icarohs7@gmail.com>
2023-03-08 09:43:58 -0300

    Fix deprecations
    Signed-off-by: Icaro Temponi <icarohs7@gmail.com>

--- lib/src/build_context_impl.dart
+++ lib/src/build_context_impl.dart
@@ -60,17 +60,13 @@ extension ThemeExt on BuildContext {
 
   TextTheme get primaryTextTheme => Theme.of(this).primaryTextTheme;
 
-  TextTheme get accentTextTheme => Theme.of(this).accentTextTheme;
-
   BottomAppBarTheme get bottomAppBarTheme => Theme.of(this).bottomAppBarTheme;
 
   BottomSheetThemeData get bottomSheetTheme => Theme.of(this).bottomSheetTheme;
 
-  Color get backgroundColor => Theme.of(this).backgroundColor;
+  Color get backgroundColor => Theme.of(this).colorScheme.background;
 
   Color get primaryColor => Theme.of(this).primaryColor;
-
-  Color get buttonColor => Theme.of(this).buttonColor;
 
   Color get scaffoldBackgroundColor => Theme.of(this).scaffoldBackgroundColor;
 
@@ -90,31 +86,31 @@ extension ThemeExt on BuildContext {
 
   bool get isLinux => this.platform == TargetPlatform.linux;
 
-  TextStyle? get headline1 => textTheme.headline1;
+  TextStyle? get headline1 => textTheme.displayLarge;
 
-  TextStyle? get headline2 => textTheme.headline2;
+  TextStyle? get headline2 => textTheme.displayMedium;
 
-  TextStyle? get headline3 => textTheme.headline3;
+  TextStyle? get headline3 => textTheme.displaySmall;
 
-  TextStyle? get headline4 => textTheme.headline4;
+  TextStyle? get headline4 => textTheme.headlineMedium;
 
-  TextStyle? get headline5 => textTheme.headline5;
+  TextStyle? get headline5 => textTheme.headlineSmall;
 
-  TextStyle? get headline6 => textTheme.headline6;
+  TextStyle? get headline6 => textTheme.titleLarge;
 
-  TextStyle? get subtitle1 => textTheme.subtitle1;
+  TextStyle? get subtitle1 => textTheme.titleMedium;
 
-  TextStyle? get bodyText1 => textTheme.bodyText1;
+  TextStyle? get bodyText1 => textTheme.bodyLarge;
 
-  TextStyle? get bodyText2 => textTheme.bodyText2;
+  TextStyle? get bodyText2 => textTheme.bodyMedium;
 
-  TextStyle? get caption => textTheme.caption;
+  TextStyle? get caption => textTheme.bodySmall;
 
-  TextStyle? get button => textTheme.button;
+  TextStyle? get button => textTheme.labelLarge;
 
-  TextStyle? get subtitle2 => textTheme.subtitle2;
+  TextStyle? get subtitle2 => textTheme.titleSmall;
 
-  TextStyle? get overline => textTheme.overline;
+  TextStyle? get overline => textTheme.labelSmall;
 }
 
 extension ScaffoldExt on BuildContext {
@@ -151,11 +147,11 @@ class _Form {
 
   final BuildContext _context;
 
-  bool validate() => Form.of(_context)!.validate();
+  bool validate() => Form.of(_context).validate();
 
-  void reset() => Form.of(_context)!.reset();
+  void reset() => Form.of(_context).reset();
 
-  void save() => Form.of(_context)!.save();
+  void save() => Form.of(_context).save();
 }
 
 extension FormExt on BuildContext {
